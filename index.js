5f290c24f7650cc54b2a9ad1798f3e01783b762a
Brandon Kim <bkim95ca@gmail.com>
2018-02-26 23:05:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ return function();
 
 
 }
-var theFunk = funkyFunction()
+var theFunk = funkyFunction();
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
